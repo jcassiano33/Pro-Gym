353cd6ffb2724cbf99ada40d4bc4baf4cf8f3b61
José Cassiano <jcnetox08@gmail.com>
2026-08-24 11:57:02 -0300

    atualizando os models (aluno)

--- progym/models.py
+++ progym/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Models):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=100)
+    data_nasc = models.DateField()
+
+    telefone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100)
+
+    senha = models.CharField(max_length=100)
+
+    foto = models.ImageField(upload_to="media")
+    bio = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.nome
