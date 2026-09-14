60e7ba6efa7fd9e40e49450cc7bc4187f95b0a46
Gustavo Ivo <gustaivo192@gmail.com>
2026-09-14 11:32:25 -0300

    Correção do /.github e preeenchimento do admin.py

--- progym/admin.py
+++ progym/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-from . models import 
-# Register your models here.
+from . models import Aluno, Academia, Treinador, Treino, Matricula, Exercicio, ExercicioTreino, RegistroTreino, AvaliacaoFisica, Dieta, AvaliacaoTreinador, SolicitacaoTreinador
+
+admin.site.register(Aluno)
+admin.site.register(Academia)
+admin.site.register(Treinador)
+admin.site.register(Treino)
+admin.site.register(Matricula)
+admin.site.register(Exercicio)
+admin.site.register(ExercicioTreino)
+admin.site.register(RegistroTreino)
+admin.site.register(AvaliacaoFisica)
+admin.site.register(AvaliacaoTreinador)
+admin.site.register(Dieta)
+admin.site.register(SolicitacaoTreinador)
--- progym/models.py
+++ progym/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from tinymce.models import HTMLField
 from django.contrib.auth.models import AbstractUser
 
 
